40f903cb00573cbcb6a2fb58baecddd608591a03
Nikita Lipnyagov <nikitalipnagov@gmail.com>
2018-04-15 16:55:56 +0300

    Task1

---
+++ Classes/Task1/Task1/Adress.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Adress
+    {
+        public String Index { get; set; }
+        public String Country { get; set; }
+        public String City { get; set; }
+        public String Street { get; set; }
+        public String House { get; set; }
+        public String Apartment { get; set; }
+
+        public Adress(){}
+
+        public Adress(String index, String country, String city, String street, String house, String apartment)
+        {
+            Index = index;
+            Country = country;
+            Street = street;
+            House = house;
+            Apartment = apartment;
+        }
+
+        public override string ToString()
+        {
+            return "Adress: " + Index +", " + Country + ", " + City + ", " + Street + ", " + House + ", " + Apartment;
+        }
+    }
+}
---
+++ Classes/Task1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adress adress = new Adress("0056","Ukraine","-","Polya","11","3a");
+            adress.City = "Dnipro";
+            Console.WriteLine(adress.ToString());
+            Console.ReadKey();
+        }
+    }
+}
