ca0c0ced31161dbe25cf5b682957a6956846676c
Nikita Lipnyagov <nikitalipnagov@gmail.com>
2018-04-16 12:41:03 +0300

    Classes Task2

---
+++ Classes/Task2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(10, 20);
+            Console.WriteLine("Area: " + rectangle.Area);
+            Console.WriteLine("Perimeter: " + rectangle.Perimeter);
+            Console.ReadKey();
+        }
+    }
+}
