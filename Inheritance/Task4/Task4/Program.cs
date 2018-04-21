ab4b705c93b3e2588008e8d14acb0762783d034d
ArturLozovskyi <lozartval@gmail.com>
2018-04-21 13:38:37 +0300

    Task4

---
+++ Inheritance/Task4/Task4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+
+
+    class DocumentWorker {
+        public virtual void OpenDocument() {
+            Console.Write("Документ открыт");
+        } 
+        public virtual void EditDocument() {
+            Console.Write("Редактирование документа доступно в версии Про");
+        }
+        public virtual void SaveDocument() {
+            Console.Write("Сохранение документа доступно в версии Про");
+        }
+    }
+
+    class ProDocumentWorker : DocumentWorker {
+        public override void EditDocument() {
+            Console.Write("Документ отредактирован");
+        }
+        public override void SaveDocument() {
+            Console.Write("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+
+    class ExpertDocumentWorker : ProDocumentWorker {
+        public override void SaveDocument() {
+            Console.Write("Документ сохранен в новом формате");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.Write("Введите ключ");
+            s = Console.ReadLine();
+            if(s!=null) {
+                DocumentWorker obj = new ProDocumentWorker();
+            }
+        }
+    }
+}
