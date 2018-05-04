2aa16f5b05f4c791039008d28bb81d3319f221b8
Nikita Lipnyagov <nikitalipnagov@gmail.com>
2018-05-04 16:08:46 +0300

    Task1, Task2

---
+++ Collections/Task1/Task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyNumberCollection numbers = new MyNumberCollection(new int[] { 1, 2, 3, 4, 5, 6 });
+
+            foreach(var item in numbers.GetEvenNumbers())
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        public class MyNumberCollection
+        {
+            private int[] array;
+
+            public MyNumberCollection(int[] array)
+            {
+                this.array = array;
+            }
+
+            public IEnumerable<int> GetEvenNumbers()
+            {
+                for (int index = 0; index < array.Length; index++)
+                {
+                    if (array[index] % 2 == 0)
+                    {
+                        yield return array[index];
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ Collections/Task2/Task2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<String> list = new MyList<string>(10);
+            list.Add("one");
+            list.Add("two");
+            list.Add("three");
+            list.Add("four");
+            list.Add("five");
+
+            foreach(var item in list.GetList())
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyList<T>
+    {
+        private static int size;
+        private T[] array;
+
+        private static int currentSize = 0;
+
+        public int Count
+        {
+            get { return currentSize; }
+        }
+
+        public T this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+
+        public MyList(int capacity)
+        {
+            size = capacity;
+            array = new T[size];
+        }
+
+        public void Add(T item)
+        {
+            if (currentSize < size)
+            {
+                array[currentSize] = item;
+                currentSize++;
+            }
+            else
+            {
+                size++;
+
+                Array.Resize<T>(ref array, size);
+                array[currentSize] = item;
+                currentSize++;
+            }
+        }
+
+        public IEnumerable GetList()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                yield return array[i];
+            }
+        }
+    }
+}
