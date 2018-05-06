1a27370396af5eb665d564a9b3e88a66a77a0683
Nikita Lipnyagov <nikitalipnagov@gmail.com>
2018-05-06 11:07:22 +0300

    Task2

---
+++ IO/Task2/Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Creating of file
+                StreamWriter textFile = new StreamWriter(Path.GetFullPath(@"text.txt"));
+                textFile.WriteLine("Text file was created!");
+                textFile.Close();
+
+                //Reading this file
+                using (StreamReader sr = new StreamReader(Path.GetFullPath(@"text.txt"), Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
